20979ec457a5a8aa2610e9e33eb28420c73282c5
mstsurnalyakter <surnalyms@gmail.com>
2024-05-19 16:04:02 +0600

    fetch data based on query parameters

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,23 +1,44 @@
 import PropTypes from 'prop-types'
+import queryString from "query-string";
+import {useNavigate, useSearchParams} from 'react-router-dom'
 
 const CategoryBox = ({ label, icon: Icon }) => {
+
+  const [params,setParams] = useSearchParams();
+  const category = params.get('category');
+
+
+   const navigate = useNavigate();
+
+  const handleClick = () =>{
+
+    let currentQuery = {category:label};
+
+    const url = queryString.stringifyUrl({
+      url:'/',
+      query:currentQuery
+    })
+    navigate(url)
+  }
+
   return (
     <div
-      className={`flex 
-  flex-col 
-  items-center 
-  justify-center 
+      onClick={handleClick}
+      className={`flex
+  flex-col
+  items-center
+  justify-center
   gap-2
   p-3
   border-b-2
   hover:text-neutral-800
   transition
-  cursor-pointer`}
+  cursor-pointer ${category === label ? 'border-b-red-500 text-red-400':undefined }`}
     >
       <Icon size={26} />
-      <div className='text-sm font-medium'>{label}</div>
+      <div className="text-sm font-medium">{label}</div>
     </div>
-  )
+  );
 }
 
 CategoryBox.propTypes = {
