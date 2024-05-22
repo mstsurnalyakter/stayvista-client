6ebc0e1704270b280f841b45f1b6731acb98ed02
mstsurnalyakter <surnalyms@gmail.com>
2024-05-22 08:36:39 +0600

    date range added

--- src/pages/DashboardPages/Host/AddRoom.jsx
+++ src/pages/DashboardPages/Host/AddRoom.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import AddRoomForm from "../../../components/Form/AddRoomForm";
 
 
 const AddRoom = () => {
+  const [dates, setDates] = useState({
+    startDate: new Date(),
+    endDate: null,
+    key: "selection",
+  });
+
+//date range handler
+  const handleDates =  item =>{
+    console.log(item);
+    setDates(item.selection);
+  }
+
   return (
     <div>
       <h2>AddRoom page</h2>
       {/* Form */}
+      <AddRoomForm dates={dates} handleDates={handleDates} />
     </div>
   );
 }
