638ad004c983c723bf29615e59d3dd6afce42361
mstsurnalyakter <surnalyms@gmail.com>
2024-05-31 16:17:33 +0600

    user role update functionality added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -4,10 +4,70 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import useAuth from '../../../hooks/useAuth'
 import avatarImg from '../../../assets/images/placeholder.jpg'
+import HostModal from '../../Modal/HostModal'
+import toast from "react-hot-toast";
+import axios from 'axios'
+import useAxiosSecure from '../../../hooks/useAxiosSecure'
 
 const Navbar = () => {
   const { user, logOut } = useAuth()
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const axiosSecure = useAxiosSecure();
+
+  //for modal
+  const [isModalOpen,setIsModalOpen] = useState(false);
+
+  const closeModal = () =>{
+   setIsModalOpen(false);
+  }
+
+  // const modalHandler = async () =>{
+  //   try {
+  //         const currentUser = {
+  //           email: user?.email,
+  //           role: "guest",
+  //           status: "Requested",
+  //         };
+  //         const { data } = await axiosSecure.put(
+  //           `/user`,
+  //           currentUser
+  //         );
+  //         if (data?.modifiedCount > 0) {
+  //           toast.success('Success! Please wait for admin confirmation')
+  //         }else{
+  //           toast.success("Please!, Wait for admin approval👊");
+  //         }
+  //         console.log(data);
+  //   } catch (error) {
+  //     toast.error(error.message);
+  //     console.log(error);
+  //   }finally{
+  //       closeModal();
+  //   }
+  // }
+
+    const modalHandler = async () => {
+      console.log("I want to be a host");
+      try {
+        const currentUser = {
+          email: user?.email,
+          role: "guest",
+          status: "Requested",
+        };
+        const { data } = await axiosSecure.put(`/user`, currentUser);
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          toast.success("Success! Please wait for admin confirmation");
+        } else {
+          toast.success("Please!, Wait for admin approval👊");
+        }
+      } catch (err) {
+        console.log(err);
+        toast.error(err.message);
+      } finally {
+        closeModal();
+      }
+    };
 
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
@@ -29,15 +89,22 @@ const Navbar = () => {
               <div className="flex flex-row items-center gap-3">
                 {/* Become A Host btn */}
                 <div className="hidden md:block">
-                  {!user && (
-                    <button
-                      disabled={!user}
-                      className="disabled:cursor-not-allowed cursor-pointer hover:bg-neutral-100 py-3 px-4 text-sm font-semibold rounded-full  transition"
-                    >
-                      Host your home
-                    </button>
-                  )}
+                  {/* {!user && ( */}
+                  <button
+                    onClick={() => setIsModalOpen(true)}
+                    // disabled={!user}
+                    className="disabled:cursor-not-allowed cursor-pointer hover:bg-neutral-100 py-3 px-4 text-sm font-semibold rounded-full  transition"
+                  >
+                    Host your home
+                  </button>
+                  {/* )} */}
                 </div>
+                {/* modal */}
+                <HostModal
+                  modalHandler={modalHandler}
+                  isOpen={isModalOpen}
+                  closeModal={closeModal}
+                />
                 {/* Dropdown btn */}
                 <div
                   onClick={() => setIsOpen(!isOpen)}
